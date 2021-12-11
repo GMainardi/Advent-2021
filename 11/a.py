7ac42c42407829a960be6e0012fb65250811dae6
Guido Mainardi <89947914+GMainardi@users.noreply.github.com>
2021-12-11 02:41:20 -0300

    day 11

---
+++ 11/a.py
@@ -0,0 +1,78 @@
+import numpy as np
+f = 0
+
+def add(mat1, mat2):
+    for x in range(len(mat)):
+        for y in range(len(mat[0])):
+            mat1[x][y] += int(mat2[x][y])
+
+def valid(x, y, maxX, maxY):
+    return x > -1 and y > -1 and x < maxX and y < maxY
+
+def flash(x, y, mat):
+    temp = np.zeros((len(mat), len(mat[0])))
+    adj = [(x-1, y), (x+1, y), (x, y-1), (x, y+1), (x-1, y-1), (x-1, y+1), (x+1, y-1), (x+1, y+1)]
+    for a, b in adj:
+        if valid(a, b, len(mat), len(mat[0])): 
+                temp[a][b] +=1
+    return temp
+
+def increase(mat):
+    new = np.zeros((len(mat), len(mat[0])))
+    flashes = np.zeros((len(mat), len(mat[0])))
+    flashed = False
+    for x in range(len(mat)):
+        for y in range(len(mat[0])):
+            mat[x][y] += 1
+            if mat[x][y] > 9 and flashes[x][y] == 0:
+                flashes[x][y] = 1
+                flashed = True
+                add(new,flash(x, y, mat))
+    add(mat, new)
+    if flashed:
+        check(mat, flashes)
+    clean_mat(mat)
+    
+
+
+def check(mat, flashes):
+    new = np.zeros((len(mat), len(mat[0])))
+    flashed = False
+    for x in range(len(mat)):
+        for y in range(len(mat[0])):
+            if mat[x][y] > 9 and flashes[x][y] == 0:
+                flashes[x][y] = 1
+                flashed = True
+                add(new,flash(x, y, mat))
+    add(mat, new)
+    if flashed:
+        check(mat, flashes)
+
+
+def clean_mat(mat):
+    global f
+    for x in range(len(mat)):
+        for y in range(len(mat[0])):
+            if mat[x][y] > 9:
+                f += 1
+                mat[x][y] = 0
+
+        
+def show_mat(mat):
+    for x in range(len(mat)):
+        for y in range(len(mat[0])):
+            print(mat[x][y], end='')
+        print()
+
+
+input =  [line.strip() for line in open('input.txt', 'r')]
+
+mat = []
+for line in input:
+    mat.append([int(value) for value in line])
+
+
+for i in range(100):
+    increase(mat)
+
+print(f)
