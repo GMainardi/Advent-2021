de19b08f1e67fa5b1091c508d8335013d7f4603b
Guido Mainardi <89947914+GMainardi@users.noreply.github.com>
2021-12-17 15:50:36 -0300

    day 17

--- 17/b.py
+++ 17/b.py
@@ -1,19 +1,17 @@
-from math import ceil, sqrt
-def shot(pos, vel):
-    pos = (pos[0] + vel[0], pos[1] + vel[1])
-    if vel[0] != 0:
-        vel = (vel[0] + 1 if vel[0] < 0 else vel[0] -1, vel[1]-1)
-    else:
-        vel = (vel[0], vel[1]-1)
-    return pos, vel
+from math import floor, ceil, sqrt
+import time
+def reverse_gauss(value):
+    delt = abs(1-(-8*value))
+    ans = (-1 + sqrt(delt))/2
+    return ans
 
-def hit(pos, start, end):
-    return (pos[0] >= start[0] and pos[0] <= end[0]) and (pos[1] <= start[1] and pos[1] >= end[1])
+def it_dict(y):
+    arr = {}
+    for it in range(2*y+2):
+        arr[it] = []
+    return arr
 
-def can_hit(pos, end):
-    return pos[0] <= end[0] and pos[1] >= end[1]
-
-input = [line.strip() for line in open('input.txt', 'r')][0]
+input = [line.strip() for line in open('input_test.txt', 'r')][0]
 
 values = input.split(' ')[2:]
 x1, x2 = values[0].replace(',', '').split('=')[1].split('..')
@@ -22,15 +20,57 @@ y2, y1 = values[1].replace(',', '').split('=')[1].split('..')
 start = (int(x1), int(y1))
 end = (int(x2), int(y2))
 hits = 0
-delt = sqrt(abs(1-(8*int(x1))))
-minx = ceil((1 + delt)/2)
-for x in range(minx-1, end[0]+1):
-    for y in range(end[1],abs(end[1])):
-        pos = (0,0)
-        vel = (x, y)
-        my = 0
-        while can_hit(pos, end) and not hit(pos, start, end):
-            pos, vel = shot(pos, vel)
-        if hit(pos, start, end):
-            hits += 1
-print(hits)
+minx = ceil(reverse_gauss(int(x1)))
+st = time.time()
+hits_x = it_dict(abs(end[0]))
+for x in range(minx, end[0]+1):
+    hit = (x + x**2)/2
+
+    if hit <= end[0]:
+        temp = hit - start[0]
+        it = x - floor(reverse_gauss(temp))
+        for i in range(it, len(hits_x.keys())):
+            hits_x[i].append(x)
+
+    else:
+        temp = hit - start[0]
+        first_hit = x - floor(reverse_gauss(temp))
+        last_hit = x - ceil(reverse_gauss(hit-end[0]))
+        if last_hit == first_hit:
+            hits_x[last_hit].append(x)
+        else:
+            for i in range(first_hit, last_hit+1):
+                hits_x[i].append(x)
+ 
+points = set([])
+for y in range(end[1],abs(end[1])):
+    if y > 0:
+        max = int((y+(y**2))/2)
+        s = max - start[1]
+        e = max - end[1]
+        first_hit = (y+1) + ceil(reverse_gauss(s))
+        last_hit = (y+1) + floor(reverse_gauss(e))
+        if first_hit == last_hit:
+            for x in hits_x[first_hit]:
+                points.add((x,y))
+        else:
+            for i in range(first_hit, last_hit+1):
+                for x in hits_x[i]:
+                    points.add((x,y))
+    else:
+        pos = 0
+        vel = y
+        it = 0
+        if y > 0:
+            it += y*2 + 1
+            vel = (y+1) * -1
+        while pos >= end[1]:
+                if pos <= start[1]:
+                    for x in hits_x[it]:
+                        points.add((x,y))
+                pos += vel
+                vel -= 1
+                it += 1
+et = time.time()
+print(et-st)
+print(len(points))
